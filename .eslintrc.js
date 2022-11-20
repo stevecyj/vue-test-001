b757156112dee86bb833f6b20c339a5932af9be2
Steve <ycsteven.tsao@gmail.com>
2022-11-21 00:04:21 +0800

    fix:eslint setting for jest syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,26 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    jest: true,
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
-}
+        jest: true,
+      },
+    },
+  ],
+};
